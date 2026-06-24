2d1d9b4c3eba342ce80a436bf2febdd030187fec
thakshila <thakshilakavindi926@gmail.com>
2026-06-24 12:58:11 +0530

    tocken fixed

--- src/index.ts
+++ src/index.ts
@@ -56,7 +56,7 @@ const corsOptions = {
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization",, "x-access-token"],
 };
 
 app.use(cors(corsOptions));
